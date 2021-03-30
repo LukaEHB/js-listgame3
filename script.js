6a4bb02b5a35305a48feb49150f73d68dc80d53d
luka <lka.v.lv@gmail.com>
2021-03-30 13:26:47 +0200

    SomeFiles

---
+++ script.js
@@ -0,0 +1,42 @@
+const SumOfList = class{
+    constructor(child, parent, total){
+      this.$child = $(child);
+      this.$parent = $(parent);
+      this.$total = $(total);
+      this.initEvents();
+      this.calcTotal();
+    }
+  
+    initEvents(){
+      this.$child.on('click',(e)=>{
+        this.appendItem(e);
+        this.calcTotal();
+      })
+    }
+  
+    appendItem(e){
+      const $clickedItem = $(e.currentTarget);
+      const $clickedParent = $clickedItem.parent();
+      const clickedLeftList = $clickedParent.is(this.$parent.first());
+  
+      if (clickedLeftList) {
+        this.$parent.last().append($clickedItem);
+      } else {
+        this.$parent.first().append($clickedItem);
+      }
+    }
+  
+    calcTotal(){
+      this.$parent.each((index, parent)=>{
+          const $children = $(parent).find(this.$child);
+          let total = 0;
+          $children.text((index, text)=>{
+              if (isNaN(parseInt(text))) return;
+              total += parseInt(text);
+          });
+          $(parent).next().find(this.$total).text(total);
+      })
+    }
+  }
+  
+  new SumOfList('li','.js-list', ".js-total");
